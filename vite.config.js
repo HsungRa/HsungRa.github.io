55bed3923aba5459450ee647018da12e98ea5931
HsungRa <m15810983447@163.com>
2024-05-15 20:52:43 +0800

    fix static resource 404

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {NodeGlobalsPolyfillPlugin} from '@esbuild-plugins/node-globals-polyfill'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [vue()],
     resolve: {
         alias: {
