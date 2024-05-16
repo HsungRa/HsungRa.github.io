85f7b61d2255eb2742586cad19627a924f5478e6
rui.xiong <xiongrui@espressif.com>
2024-05-16 16:56:53 +0800

    update

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,18 +4,16 @@ import {useRouter} from 'vue-router'
 import {ROUTER_BLOG_PANEL} from "../router/router_names.js";
 
 const router = useRouter();
-// path: `/${ROUTER_BLOG_PANEL}/
 const goto = (category) => {
   console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>goto:"+category)
   return router.push(`/${ROUTER_BLOG_PANEL}/${category}`)
 }
-const buildHerf = (category) => {
 
+const buildHerf = (category) => {
   return "/panel/" + category
 }
 </script>
-<!--        :href=buildHerf(index)
--->
+<!--:href=buildHerf(index)-->
 <template>
   <el-space direction="vertical" style="width: 100%" fill fill-ratio="80">
     <VaCard stripe stripe-color="success" v-for="(category, index) of CATEGORY_CONFIG" :key="index" gradient>
